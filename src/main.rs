0fc0519f5adfe4e7524b4bf0a6db1d94fd0aa061
rizkijanuari <rizkijanuar.work@gmail.com>
2026-04-28 11:07:26 +0700

    FEAT[r]: (basic) materi comment

--- src/main.rs
+++ src/main.rs
@@ -16,3 +16,21 @@ fn materi_variable() {
     age = 24;
     println!("{}", age);
 }
+
+
+
+#[test]
+fn materi_comment() {
+    // 1. Ini namanya Single Line Comment (pake dua garis miring)
+    // Biasanya buat catatan pendek di atas baris kode.
+
+    let skor = 100; // Bisa juga diletakan di samping kode kayak gini
+
+    /* 2. Ini namanya Multi-line Comment.
+       Bisa buat catatan yang panjang lebar
+       sampai berbaris-baris.
+    */
+
+    println!("Cek terminal, komentar nggak bakal muncul di sini!");
+    println!("Skor gue: {skor}");
+}
