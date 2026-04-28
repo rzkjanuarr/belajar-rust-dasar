3cbf70be688aadd9deb5da39f62bb95f7a5ce129
rizkijanuari <rizkijanuar.work@gmail.com>
2026-04-28 11:03:11 +0700

    FEAT[r]: (basic) materi variable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
     println!("Hello, world!");
 }
+
+
+#[test]
+fn materi_variable() {
+    // 1. Imutable = Variable yang tidak dapat dirubah!
+    let name = "Rizki Januar I.";
+    println!("{}", name);
+
+    // 2. Mutable = Variable yang dapat dirubah!
+    let mut age = 20;
+    println!("{}", age);
+
+    age = 24;
+    println!("{}", age);
+}
